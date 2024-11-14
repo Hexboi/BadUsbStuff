6e388a75d58e94a0b0886fd7631859e36bd2b793
Hexboi <68022295+Hexboi@users.noreply.github.com>
2024-11-15 00:58:23 +0100

    fixed comments

--- Wifi/Extractor.ps1
+++ Wifi/Extractor.ps1
@@ -1,6 +1,6 @@
-#Finding the USB Drive: The command Get-WmiObject queries for devices with DriveType = 2, which represents removable storage. Replace "USB_LABEL" with the actual label of your USB drive.
-#Saving the Output: Once the USB drive is detected, the script saves test.txt with Wi-Fi profiles, passwords, and user information directly to the USB.
-#This script will exit with a message if it doesn’t find a USB drive with the specified label.
+# The command Get-WmiObject queries DriveType = 2, which represents removable storage. 
+# Replace "USB_LABEL" with the actual label of your USB drive.
+# Once the USB drive is detected, the script uses test.txt as output file.
 
 $usbDrive = Get-WmiObject -Query "SELECT * FROM Win32_Volume WHERE DriveType = 2" | Where-Object { $_.Label -eq "USB_LABEL" }
 
